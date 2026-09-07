0bd8dfe334ab13ca34db5e46236ab7456c2cd6cb
18344934977sail-pixel <18344934977sail@gmail.com>
2026-09-08 02:45:22 +0800

    Refresh mobile app offline cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='schedule-ios-polish-v8';
+const CACHE='schedule-mobile-app-v10';
 const ASSETS=['./','./index.html','./app.css','./schedule-data.js','./app.js','./manifest.webmanifest','./logo.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
